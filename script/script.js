5e21b7b46c5a37bf76d58e8f62953fe355d5f913
wilPoly <misterwrinkle@gmail.com>
2017-08-27 22:23:29 +0200

    Done for tonight

--- script/script.js
+++ script/script.js
@@ -2,10 +2,9 @@ $(document).ready(function(){
 
 	function makeGrid(){
 		var gridSize = 16;
-		for (var i = 0; i < 16; i++) {
+		for (var i = 0; i < gridSize; i++) {
 			$('#container').append(document.createElement('div'));
 			$('#container').find('div').addClass('gridBlock');
-			console.log('added')
 		}
 	}
 
