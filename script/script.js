5876eff294c88fc50ceb86b71f3fd9a84a5896ff
wilPoly <misterwrinkle@gmail.com>
2017-08-29 23:14:13 +0200

    Create a 16 square grid using the generate button

--- script/script.js
+++ script/script.js
@@ -1,13 +1,21 @@
+function makeGrid(gridSize){
+	//Create divs with 'gridBlock' class
+	for (var i = 0; i < gridSize **2; i++) {
+		$('<div>').appendTo('#container').addClass('gridBlock');
+	}
+	//Resize the divs depending on the grid-size
+	var containerWidth = $('#container').width();
+	var gridBlockSize =  containerWidth / gridSize;
+	$('.gridBlock').css({'width': gridBlockSize, 'height': gridBlockSize});
+	console.log(containerWidth);
+	console.log(gridBlockSize);
+}
+
+
 $(document).ready(function(){
 
-	function makeGrid(){
-		var gridSize = 16;
-		for (var i = 0; i < gridSize; i++) {
-			$('#container').append(document.createElement('div'));
-			$('#container').find('div').addClass('gridBlock');
-		}
-	}
-
-	$('#generate').on('click', makeGrid);
+	$('#generate').on('click', function(){
+		makeGrid(16);
+	});
 	
 });
